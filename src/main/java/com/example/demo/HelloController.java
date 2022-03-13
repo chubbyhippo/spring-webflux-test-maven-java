6c81286e51ea280214e4d1ecd22c8f1d65b4a538
ch <mkmsacc@outlook.com>
2022-03-13 10:02:04 +0700

    Add Controller

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("hello")
+    public Mono<String> greet() {
+        return helloService.greet();
+    }
+}
