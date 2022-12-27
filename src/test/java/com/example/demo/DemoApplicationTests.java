4d22522abad3d78f0552c5fbb435b6512922394c
ch <mkmsacc@outlook.com>
2022-12-27 09:58:02 +0700

    Remove AutoConfigureWebTestClient

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,26 +2,24 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
 class DemoApplicationTests {
 
-	@Autowired
-	private WebTestClient webTestClient;
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@Test
-	void shouldReturnHello() {
-		webTestClient.get()
-				.uri("/hello")
-				.exchange()
-				.expectStatus()
-				.is2xxSuccessful()
-				.expectBody(String.class)
-				.isEqualTo("hello");
-	}
+    @Test
+    void shouldReturnHello() {
+        webTestClient.get()
+                .uri("/hello")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .isEqualTo("hello");
+    }
 
 }
